8550de4d2b0462533290c24abcade71d031c1f86
qwerasdfxzdcv <cshxx992@gmail.com>
2024-11-01 13:56:32 +0900

    BaseURL 수정

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,32 @@ const initProduct = {
   name:"",
   price:0,
 }
-axios.dafault.baseURL = import.meta.env.PROD ? location.hostname : "http://localhost:8080"
+const getBaseURL = ()=>{
+  return import.meta.env.PROD ? `http://${location.hostname}` : "http://localhost:8080";
+}
 const getAllProductsApi = async () =>{
   const { data } = await axios({
     method:"GET",
-    url:"/api/v1/products"
+    url:`${getBaseURL()}/api/v1/products`
   })
   return data
 }
 const sellingApi = async(id) => {
   const {data} = await axios({ 
     method:"PUT",
-    url:`/api/v1/products/${id}/selling`
+    url:`${getBaseURL()}/api/v1/products/${id}/selling`
   })
 }
 const recevingApi = async(id) => {
   const {data} = await axios({
     method:"PUT",
-    url:`/api/v1/products/${id}/receving`
+    url:`${getBaseURL()}/api/v1/products/${id}/receving`
   })
 }
 const addProductApi = async (product) => {
   const {data} = await axios({
     method: "POST",
-    url: `/api/v1/products`,
+    url: `${getBaseURL()}/api/v1/products`,
     data: product
   })
 }
